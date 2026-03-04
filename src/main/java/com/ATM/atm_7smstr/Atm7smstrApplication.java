e458dc61c0b6f5ffa2f363439d3451a4009231c6
Marvin Siles <marvin.siles.mejia.m@gmail.com>
2026-03-04 04:46:29 -0600

    Inicializa proyecto Spring Boot ATM

---
+++ src/main/java/com/ATM/atm_7smstr/Atm7smstrApplication.java
@@ -0,0 +1,13 @@
+package com.ATM.atm_7smstr;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atm7smstrApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atm7smstrApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ATM/atm_7smstr/Atm7smstrApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ATM.atm_7smstr;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atm7smstrApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
